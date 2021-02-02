79e40fc9f8f81395062c8c50910de325617276b4
Orna Lipkin <fifilp@gmail.com>
2021-02-02 21:51:23 +0200

    up and runnnig

--- graph.py
+++ graph.py
@@ -1,4 +1,7 @@
 class GRAPH:
+    # adjMatrix = None
+    # size = 0
+
     # Initialize the matrix
     def __init__(self, size):
         self.adjMatrix = []
@@ -9,7 +12,7 @@ class GRAPH:
     # Add edges
     def addEdge(self, v1, v2):
         if v1 == v2:
-            print("Same vertex %d and %d" % (v1, v2))
+            return
         self.adjMatrix[v1][v2] = 1
 
     # Remove edges
@@ -18,15 +21,33 @@ class GRAPH:
             print("No edge between %d and %d" % (v1, v2))
             return
         self.adjMatrix[v1][v2] = 0
-        self.adjMatrix[v2][v1] = 0
-
-    def __len__(self):
-        return self.size
 
     # Print the matrix
+
     def printGraph(self):
         for i in range(self.size):
             for j in range(self.size):
-                print(self.adjMatrix[i][j], end = " ")
-            print() 
-        
+                print(self.adjMatrix[i][j], end=" ")
+            print()
+
+    def getVertexAdjList(self, v):
+        adjList = []
+        for i in range(self.size):
+            if self.adjMatrix[v][i] == 1:
+                adjList.append(i)
+
+        return adjList
+
+    def getIndegreeList(self):
+        indegree = []
+
+        for i in range(self.size):
+            indegree.append(0)
+
+        # traverse columns and not rows - delete later
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.adjMatrix[j][i] == 1:
+                    indegree[i] += 1
+
+        return indegree
--- main.py
+++ main.py
@@ -1,69 +1,109 @@
 
 import re
+import queue
 from action import ACTION
 from graph import GRAPH
 
-def getAction(actionStr):    
-    actionStr = actionStr.strip() 
-    obj  = re.match(r'(?P<AcType>[R|W])(?P<TrNum>\d+)\((?P<AcItem>.*)\)', actionStr)
-    AcType = obj.group(1)
-    TrNum = int(obj.group(2))
-    AcItem = obj.group(3)
 
-    return ACTION(AcType,TrNum,AcItem)
+def getAction(actionStr):
+    actionStr = actionStr.strip()
+    obj = re.match(
+        r'(?P<AcType>[R|W])(?P<TrNum>\d+)\((?P<AcItem>.*)\)', actionStr)
+    acType = obj.group("AcType")
+    trNum = int(obj.group("TrNum"))
+    acItem = obj.group("AcItem")
+
+    return ACTION(acType, trNum, acItem)
 
 
 def parseSchedule(schedStr):
-    actionList=[]
+    actionList = []
 
     schedStr = schedStr.strip()
     scheduleList = schedStr.split(';')
-    
-    for i in range(len(scheduleList)):        
-        actionList.append(getAction(scheduleList[i]))
-        print(actionList[i])
- 
+
+    for actionStr in scheduleList:
+        actionList.append(getAction(actionStr))
+
     return actionList
-   
+
 
 def buildPrecGraph(schedList):
-    writeFlag = False
-    n=len(schedList)
+    n = len(schedList)
 
-    #initialize graph
-    matrixSize =findMaxTransNum(schedList)    
-    precGraph = GRAPH(matrixSize+1) # will simply ignore the 0 row/column
+    # initialize graph
+    matrixSize = findMaxTransNum(schedList)
+    precGraph = GRAPH(matrixSize)  # we'll simply ignore the 0 row/column
 
-    #add edges as needed per precedence
+    # add edges as needed per precedence
     for i in range(n):
         currTransNum = schedList[i].transactionNum
         currItem = schedList[i].item
         currType = schedList[i].actionType
-           
-        for j in range(i+1,n):
-            if (schedList[j].transactionNum != currTransNum) and (schedList[j].item == currItem) and (currType == 'W' or schedList[j].actionType == 'W'):
-                    precGraph.addEdge(currTransNum,schedList[j].transactionNum)
 
-    return precGraph               
+        for j in range(i + 1, n):
+            if (schedList[j].transactionNum != currTransNum) \
+                and (schedList[j].item == currItem) \
+                    and (currType == 'W' or schedList[j].actionType == 'W'):
+                precGraph.addEdge(currTransNum - 1,
+                                  schedList[j].transactionNum - 1)
+
+    return precGraph
 
 
 def findMaxTransNum(schedList):
-    max=0
+    max = 0
 
-    for i in range(len(schedList)):
-        max = schedList[i].transactionNum if schedList[i].transactionNum > max else max
+    for action in schedList:
+        max = action.transactionNum if action.transactionNum > max else max
 
     return max
 
 
-def main():  
-    scheduleStr = input("Enter an action schedule: ")
-    scheduleList = parseSchedule(scheduleStr)   
-    precGraph = buildPrecGraph(scheduleList)
-    precGraph.printGraph()
+def TopologicalSort(graph):
+    topoSortList = []
 
-    #check graph for cycle
-    # if no cycle, return topological sort
+    indegree = graph.getIndegreeList()
+
+    # creating queue of sources
+    sourceQ = queue.Queue(graph.size)
+    for i in range(len(indegree)):
+        if indegree[i] == 0:
+            sourceQ.put(i)
+
+    while not sourceQ.empty():
+        u = sourceQ.get()
+        # +1 to convert from matrix index to transactionNum
+        topoSortList.append(f'T{u + 1}')
+        uAdjList = graph.getVertexAdjList(u)
+        for v in uAdjList:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                sourceQ.put(v)
+
+    for degree in indegree:
+        if degree != 0:
+            return []
+
+    return topoSortList
+
+
+def main():
+    # read schedule from user
+    scheduleStr = input("Enter an action schedule: ")
+    # parse schedule and return a list of action objects
+    scheduleList = parseSchedule(scheduleStr)
+    # build precedence graph by scanning the schedule for conflicts
+    precGraph = buildPrecGraph(scheduleList)
+    precGraph.printGraph() # delete later - only for debugging
+    # topologicaly sort precedence graph.
+    sortedList = TopologicalSort(precGraph)
+    # the function will return a list of the sorted transactions
+    # or an empty list if there's a cycle in the graph
+    if sortedList:
+        print(*sortedList, sep="->")
+    else:        
+        print("NO")
 
 
 if __name__ == "__main__":
